5e91f47a35e1722fb8b94831c80161cb1013cfaa
ofolwarzny <folwarznyoliver@gmail.com>
2018-06-09 20:43:47 +0200

    add blur and pixel mutilation filter

--- src/Filter/BlurFilter.java
+++ src/Filter/BlurFilter.java
@@ -1,17 +1,38 @@
 package Filter;
 
+import java.util.ArrayList;
+
 public class BlurFilter extends AreaFilter {
 
     private int radius;
+
     public BlurFilter(int radius) {
         this.radius = radius;
     }
 
     @Override
     protected int calculate(int[] pixel, int[] maskPixel, int index, int width, int height) {
-        int colorPixel = pixel[index];
-        int minBoundX = pixel[index-radius];
-        int maxBoundX = pixel[index+radius];
-        return super.calculate(pixel, maskPixel, index, width, height);
+        ArrayList<Integer> neededForProcess = new ArrayList<>();
+        int heightPosPix = index / width;
+        int widthPosPix = index % width;
+        for (int i = heightPosPix - radius; i <= heightPosPix + radius; i++) {
+            for (int j = widthPosPix - radius; j <= widthPosPix + radius; j++) {// i * width + j
+                if(i >= 0 && i < height && j >= 0 && j < width) {
+                    neededForProcess.add(pixel[i * width + j]);
+                }
+            }
+        }
+        int valueRed = 0;
+        int valueGreen = 0;
+        int valueBlue = 0;
+        for (int it = 0; it < neededForProcess.size(); it++) {
+            valueRed += (neededForProcess.get(it) >> 16) & 0xFF;
+            valueGreen += (neededForProcess.get(it) >> 8) & 0xFF;
+            valueBlue += (neededForProcess.get(it)) & 0xFF;
+        }
+        valueRed = Math.round(valueRed / (float)neededForProcess.size());
+        valueGreen = Math.round(valueGreen / (float)neededForProcess.size());
+        valueBlue = Math.round(valueBlue / (float)neededForProcess.size());
+        return (valueRed << 16) | (valueGreen << 8) | (valueBlue);
     }
 }
