bcb71f5cb22789ab63e7ee249a5425548f201a7f
ofolwarzny <folwarznyoliver@gmail.com>
2018-06-09 21:20:28 +0200

    add imageHelper class, for filling the pixel arrays of the image and mask

---
+++ src/Filter/ImageHelper.java
@@ -0,0 +1,18 @@
+package Filter;
+
+import java.awt.image.BufferedImage;
+
+public class ImageHelper {
+
+    public static void getRGBValues(int[] pixel, int[] maskPixel, boolean maskSet, BufferedImage image1, BufferedImage image2) {
+        int width = image1.getWidth();
+        for (int i = 0; i < image1.getHeight(); i++) {
+            for (int j = 0; j < width; j++) {
+                pixel[i * width + j] = image1.getRGB(j, i);
+                if (maskSet) {
+                    maskPixel[i * width + j] = image2.getRGB(j, i);
+                }
+            }
+        }
+    }
+}
--- src/Filter/PixelFilter.java
+++ src/Filter/PixelFilter.java
@@ -21,14 +21,7 @@ public class PixelFilter implements Filter {
             processedPixel = new int[pixel.length];
             maskPixel = new int[pixel.length];
 
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    pixel[i * width + j] = image1.getRGB(j, i);
-                    if (maskIsSet) {
-                        maskPixel[i * width + j] = image2.getRGB(j, i);
-                    }
-                }
-            }
+            ImageHelper.getRGBValues(pixel,maskPixel,maskIsSet,image1,image2);
 
             int i = 0;
             for (int specPixel : pixel) {
